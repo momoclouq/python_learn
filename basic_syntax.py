18ff49df86822fb2b599be242e653d1e4a1a43f1
momoclouq <phamhoangminh050899@gmail.com>
2022-05-24 22:30:52 +0700

    first commit

---
+++ basic_syntax.py
@@ -0,0 +1,26 @@
+#printing
+print("Hello world");
+
+#variables and types
+#completely object oriented, not 'statically typed'
+#every variable is an object
+myint = 7;
+print(myint);
+
+myfloat = 7.3;
+print(myfloat);
+
+myfloat2 = float(7);
+print(myfloat2);
+
+#strings
+mystring = 'Hello'
+mystring2 = "Hello"
+mystring3 = "Hello 'welcome'"
+print(mystring + '-' + mystring2, mystring3);
+
+a, b=1, 2;
+print(a, b);
+
+print(mystring + b + myfloat2);
+
