c230587f05a632a7dadd18b10a41f7e2e8908795
momoclouq <phamhoangminh050899@gmail.com>
2022-06-13 11:57:10 +0700

    add progress

--- basic/basic_syntax.py
+++ basic/basic_syntax.py
@@ -1,3 +1,4 @@
+print()
 # #printing
 # print("Hello world");
 
@@ -39,32 +40,103 @@
 # for x in mylist:
 #     print("loop: " + str(x));
 
-#list exercises
-numbers = [];
-strings = [];
-names = ['minh', 'loco', 'lbolo'];
+# list exercises
+# numbers = [];
+# strings = [];
+# names = ['minh', 'loco', 'lbolo'];
 
-for i in range(3):
-    numbers.append(i + 1);
+# for i in range(3):
+#     numbers.append(i + 1);
 
-for number in numbers:
-    strings.append(str(number) + " value");
+# for number in numbers:
+#     strings.append(str(number) + " value");
 
-print(numbers);
-print(strings);
+# print(numbers);
+# print(strings);
 
-# operators
-value = 1 + 3 + 5;
-print(value);
+# # operators
+# value = 1 + 3 + 5;
+# print(value);
 
-value2 = 3 ** 4;
-print(value2);
+# value2 = 3 ** 4;
+# print(value2);
 
-lotsofhellos = "hello" * 10;
-print(lotsofhellos);
+# lotsofhellos = "hello" * 10;
+# print(lotsofhellos);
 
-#concatenate list
-even_numbers = [2,4,6,8];
-odd_numbers = [1,3,5,7];
-all_numbers = odd_numbers + even_numbers;
-print(all_numbers);
+# #concatenate list
+# even_numbers = [2,4,6,8];
+# odd_numbers = [1,3,5,7];
+# all_numbers = odd_numbers + even_numbers;
+# print(all_numbers);
+
+# string formatting
+# name = "James";
+# name2 = "Minh";
+# num = 12;
+# print('name: %s' % (name));
+# print('name1: %s, name2: %s' % (name, name2));
+# print('name1: %s, name2: %s, num1: %d' % (name, name2, num));
+# # basic format:
+# # %s string (or any object with a string representation)
+# # %d Integers
+# # %f float
+# # %.4f - float with fixed amount of digits to the right of the dot
+# # %x/%X integer in hex representation
+# print();
+
+# data = ("John", "Doe", 53.44);
+# format_string = "Hello %s %s. Your current balanace is $%.2f.";
+# print(format_string % data);
+
+# string operations
+# num1 = 1;
+# num2 = 3;
+# string = "abcdef abcde efghk";
+# print(len(string));
+# print(string.count('a'));
+# print(string.find('b'));
+# print(string[num1:num2]);
+# print(string.lower());
+# print(string.upper())
+
+# #reverse a string
+# print(string[::-1]);
+# print(string.startswith("abc"));
+# print(string.endswith("def"));
+# print(string.split(" "));
+
+s = "Hey thera! what shou"
+# Length should be 20
+print("Length of s = %d" % len(s))
+
+# First occurrence of "a" should be at index 8
+print("The first occurrence of the letter a = %d" % s.index("a"))
+
+# Number of a's should be 2
+print("a occurs %d times" % s.count("a"))
+
+# Slicing the string into bits
+print("The first five characters are '%s'" % s[:5])  # Start to 5
+print("The next five characters are '%s'" % s[5:10])  # 5 to 10
+print("The thirteenth character is '%s'" % s[12])  # Just number 12
+print("The characters with odd index are '%s'" % s[1::2])  # (0-based indexing)
+print("The last five characters are '%s'" % s[-5:])  # 5th-from-last to end
+
+# Convert everything to uppercase
+print("String in uppercase: %s" % s.upper())
+
+# Convert everything to lowercase
+print("String in lowercase: %s" % s.lower())
+
+# Check how a string starts
+if s.startswith("Str"):
+    print("String starts with 'Str'. Good!")
+
+# Check how a string ends
+if s.endswith("ome!"):
+    print("String ends with 'ome!'. Good!")
+
+# Split the string into three separate strings,
+# each containing only a word
+print("Split the words of the string: %s" % s.split(" "))
---
+++ basic/basic_syntax2.py
@@ -0,0 +1,43 @@
+# print();
+
+# name = "minh"
+# age = 23
+# if name == "minh" and age == 23:
+#     print("Your name is Minh and 23 years old");
+
+# name2 = "tom"
+# if name2 in ['tom1', 'jerry']:
+#     print("you are tom and jerry");
+# elif name in ['minh']:
+#     print("Something");
+# else:
+#     print("Helllo")
+
+
+# print();
+
+# numbers = [
+#     951, 402, 984, 651, 360, 69, 408, 319, 601, 485, 980, 507, 725, 547, 544,
+#     615, 83, 165, 141, 501, 263, 617, 865, 575, 219, 390, 984, 592, 236, 105, 942, 941,
+#     386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345,
+#     399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217,
+#     815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717,
+#     958, 609, 842, 451, 688, 753, 854, 685, 93, 857, 440, 380, 126, 721, 328, 753, 470,
+#     743, 527
+# ]
+
+# # your code goes here
+# for number in numbers:
+#     if number == 237:
+#         break;
+#     if number % 2 == 0:
+#         print(str(number) + "-");
+
+def sum_of_two(a, b):
+    return a + b;
+
+print(sum_of_two(12, 13));
+
+def list_benefits():
+    return ["More code", "More readable", "Easier code"];
+    
